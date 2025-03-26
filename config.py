6b2998e5080e9c3b327465d3c53f2d34603cb6f9
GojoSanji21 <hiluhu7@gmail.com>
2025-03-27 00:26:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6685624393"))
 SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "-1002167572229")
 
 #Port
-PORT = os.environ.get("PORT", "8080")
+PORT = os.environ.get("PORT", "156")
 
 #Database --⚠️ REQUIRED--
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://AnimeRavenBots:AnimeRavenBots@animeravenbots.huekk.mongodb.net/?retryWrites=true&w=majority&appName=FileTestingBot")
